baa6c3f141de334842de8575ffb13faaa3cc0a7e
losfair <zhy20000919@hotmail.com>
2017-10-15 15:46:22 +0800

    Debug code

--- tools/cctest.py
+++ tools/cctest.py
@@ -1,6 +1,10 @@
 import core
 import argparse
 
+def print_buf(buf, len):
+    data = [buf[i] for i in range(len)]
+    print(bytes(data))
+
 def test(data):
     cc = core.lib.unimess_config_chain_load(data, len(data))
     if cc == core.ffi.NULL:
@@ -10,14 +14,20 @@ def test(data):
     print("Load OK")
 
     test_input = "Hello world".encode()
+    #print(test_input)
+
     enc_out_len = core.ffi.new("unsigned int *")
     enc_out = core.lib.unimess_protocol_chain_encode_packet(pc, enc_out_len, test_input, len(test_input))
     if enc_out == core.ffi.NULL:
         raise Exception("Encode failed")
+    #print_buf(enc_out, enc_out_len[0])
+
     dec_out_len = core.ffi.new("unsigned int *")
     dec_out = core.lib.unimess_protocol_chain_decode_packet(pc, dec_out_len, enc_out, enc_out_len[0])
     if enc_out == core.ffi.NULL:
         raise Exception("Encode failed")
+
+    #print_buf(dec_out, dec_out_len[0])
 
     if dec_out_len[0] != len(test_input):
         raise Exception("Length mismatch")

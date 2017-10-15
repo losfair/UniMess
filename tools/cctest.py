3d3911ad1563054c3cb7d97128101617df4a663e
losfair <zhy20000919@hotmail.com>
2017-10-15 15:42:51 +0800

    Encode & decode test

--- tools/cctest.py
+++ tools/cctest.py
@@ -5,8 +5,27 @@ def test(data):
     cc = core.lib.unimess_config_chain_load(data, len(data))
     if cc == core.ffi.NULL:
         raise Exception("Config chain load failed")
+    pc = core.lib.unimess_config_chain_get_protocol_chain(cc)
     core.lib.unimess_config_chain_destroy(cc)
-    print("OK")
+    print("Load OK")
+
+    test_input = "Hello world".encode()
+    enc_out_len = core.ffi.new("unsigned int *")
+    enc_out = core.lib.unimess_protocol_chain_encode_packet(pc, enc_out_len, test_input, len(test_input))
+    if enc_out == core.ffi.NULL:
+        raise Exception("Encode failed")
+    dec_out_len = core.ffi.new("unsigned int *")
+    dec_out = core.lib.unimess_protocol_chain_decode_packet(pc, dec_out_len, enc_out, enc_out_len[0])
+    if enc_out == core.ffi.NULL:
+        raise Exception("Encode failed")
+
+    if dec_out_len[0] != len(test_input):
+        raise Exception("Length mismatch")
+    for i in range(dec_out_len[0]):
+        if dec_out[i] != test_input[i]:
+            raise Exception("Data mismatch")
+
+    print("Data OK")
 
 parser = argparse.ArgumentParser(
     description = "Config chain generator for UniMess"
